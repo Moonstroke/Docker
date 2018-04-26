de49159b6126d94f39c6d8c3273241028bfa0c38
joH1 <jiema@live.fr>
2018-04-26 18:52:16 +0200

    Add Dockerfile for the projects Clog & CUTE
    A directory is added with files to build a Docker image containing the
    projects [Clog](https://github.com/Moonstroke/Clog), a simple logging
    system in C, and [CUTE](https://github.com/Moonstroke/CUTE), which is a
    unit testing engine in C.
    
    CUTE cannot be used on its own because it uses the other project to
    work.
    
    Simply run:
        docker build clog+cute
    
    from the root of the repository to build the image.

---
+++ clog+cute/install_deps.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This script installs all the required Git repositories from their remote URIs
+# given in an external file whose name must be given as first parameter to this
+# script
+
+
+# This function installs the repo whose remote is given as parameter.
+# Functions declared in round parens are run in their own subshell.
+function install_repo() (
+	cd "$1"
+	make
+	make install
+)
+
+# This functions globally handles a Git remote URI.
+function handle_uri() {
+	local -l dir="${1##*/}"
+	echo -n "Fetching \"$1\" into \"$dir\"..."
+	git clone -q "$1" "$dir"
+	echo " Done."
+	echo -n "Installing..."
+	install_repo "$dir"
+	echo " Done."
+	rm -rf "$dir"
+	echo "Directory \"$dir\" removed."
+}
+
+
+while read -r line; do
+	if [ ${#line} -ne 0 ] && [ "${line:0:1}" != '#' ]; then
+		handle_uri $line
+	fi
+done < "$1"
+echo "Done."
